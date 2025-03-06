38ffc5c06e4f0d3151c04c1bfc0ac8b91beebff2
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2025-03-06 18:36:29 -0300

    fix: update mermaid.js version and polyfill missing methods for iOS Safari 14

--- src/libs/mermaid.js
+++ src/libs/mermaid.js
@@ -4,10 +4,25 @@ module.exports = (eleventyConfig, options) => {
     theme: 'default',
     loadOnSave: true
   };
-  let src = options?.mermaid_js_src || "https://cdn.jsdelivr.net/npm/mermaid@10.4.0/dist/mermaid.esm.min.mjs";
+  let src = options?.mermaid_js_src || "https://cdn.jsdelivr.net/npm/mermaid@11.4.1/dist/mermaid.esm.min.mjs";
 
   eleventyConfig.addLiquidShortcode("mermaid_js_scripts", () => {
-    return `<script type="module" async>
+    return `<script>
+              /* fix mermaid not loading in Safari14 since 10.5.0-alpha.1 */
+              // https://caniuse.com/?search=structuredClone
+              if (!window.structuredClone) {
+                window.structuredClone = function structuredClone(obj) {
+                  return JSON.parse(JSON.stringify(obj))
+                }
+              }
+              // https://caniuse.com/?search=hasown
+              if (!Object.hasOwn) {
+                Object.hasOwn = function hasOwn(o, k) {
+                  return Object.prototype.hasOwnProperty.call(o, k)
+                }
+              }
+            </script>
+            <script type="module" async>
               import mermaid from "${src}";
               const config = ${JSON.stringify(mermaid_config)};
               config.theme = isDarkMode() ? 'dark' : 'default';
